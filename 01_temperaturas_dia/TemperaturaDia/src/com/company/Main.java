cd05faa1c093e20f94094980cd989be404c550ab
Unknown <erlantz.smr@gmail.com>
2016-11-14 20:40:20 +0100

    Inicio ejercicio 1

---
+++ 01_temperaturas_dia/TemperaturaDia/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
