6327bdc31125ed6191538c06d7a973ad2466e962
Unknown <erlantz.smr@gmail.com>
2016-11-28 21:12:15 +0100

    Ejercicio 4 terminado

---
+++ 04_matriz_3x3/E4_matriz/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        // int[][] = new int[3][3];
+        int[][] matriz;
+        matriz = new int[3][3];
+
+
+        // Insertar valores en matriz
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Introduce valor: ");
+                matriz[i][j] = Integer.parseInt(br.readLine());
+            }
+        }
+
+        // Mostrar matriz
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.format("%4d", matriz[i][j]);
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
